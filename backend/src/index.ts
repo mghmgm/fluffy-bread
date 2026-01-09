30e23b7864f5dc4175b21464d35451b5c789abbe
mihailshattt <mihailshattt@gmail.com>
2026-01-09 21:38:01 +0300

    Move ts-node to dependencies for Render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,11 @@ dotenv.config();
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
